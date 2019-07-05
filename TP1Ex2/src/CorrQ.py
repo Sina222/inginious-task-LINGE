6465f303055157b162317d22f1e729b3c95c1c60
fiset <alexandre.fiset@student.uclouvain.be>
2019-07-05 13:18:34 +0200

    change name of a task + add TP1Ex2

--- TP1Ex2/src/CorrQ.py
+++ TP1Ex2/src/CorrQ.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-def mirroir(s):
-    return s[::-1]
+def moyenne(a,b):
+    return (a+b)/2
+
+def main():
+    a = int(input("Entrez le premier nombre :"))
+    b = int(input("Entrez le deuxième nombre :"))
+    print("La moyenne est :" + str(moyenne(a,b)))
--- TP1Ex2/src/TestQ.py
+++ TP1Ex2/src/TestQ.py
@@ -6,79 +6,48 @@ import unittest
 import q
 import CorrQ as corr
 import random
-import string
+from unittest.mock import patch
+import sys
 
-#set of string
-string_set = ["Kerys Andersen",
-"Faheem Dickerson",
-"Amaan Forrest",
-"Arvin Wardle",
-"Shanna Brown",
-"Ayyub Witt",
-"Rodrigo Haas",
-"Georgia Jackson",
-"Ehsan Rivas",
-"Habib Mccormack",
-"Rianna Monroe",
-"Felix Novak",
-"Zayaan Bloom",
-"Kanye Davila",
-"Curtis Hardin",
-"Tala Burt",
-"Cataleya Lyon",
-"Harlan Hutchings",
-"Alma Stubbs",
-"Alysia Woodard",
-"Tamika Butt",
-"Adeline Goff",
-"Imaan Armitage",
-"Ruby-May Woodley",
-"Alisa Rice",
-"Lucie Kearney",
-"Alana Reese",
-"T-Jay Broadhurst",
-"Pearl Mcfarland",
-"Korey Bridges",
-"Nora Kirby",
-"Mahamed Glass",
-"Zachariah Fraser",
-"Tristan Knight",
-"Jez Powell",
-"Samera Forbes",
-"Kier Orozco",
-"Ezmae Short",
-"Sherry Mccabe",
-"Aras Haworth",
-"Monique Flowers",
-"Ava-Mai Gill",
-"Ceara Higgs",
-"Desiree Garcia",
-"Arabella Bannister",
-"Caine Burks",
-"Sania Hastings",
-"Yu Fisher",
-"Zayne Shaffer",
-"Cheyenne O'Gallagher"]
+#Creation of the random number for the test
+a_pos = random.randint(0,10)
+b_pos = random.randint(0,10)
+a_neg = random.randint(-10,0)
+b_neg = random.randint(-10,0)
+test_pos = [a_pos,b_pos]
+test_neg = [a_neg,b_neg]
+test_pos_neg = [a_pos,b_neg]
 
-#Creation of the random string s
-s = random.choice(string_set)
-s_random = (s,_(" You did not reverse the string : {} \n Your result : {} \n Expected result : {}"))
-s_empty = ("",_("You did not pass the test for an empty string, you should return the empty string."))
-s_one = (random.choice(string.ascii_lowercase),_("You did not pass the test for one letter : {}. \n Your result : {} \n Expected result : {}"))
+original = sys.stdout
 
-TEST = [s_random,s_empty,s_one]
+TEST = [test_pos,test_neg,test_pos_neg]
 
 
 class TestPageRank(unittest.TestCase):
 
 	def test_exist_name(self):
-		self.assertTrue(hasattr(q, 'mirroir'), _("You did not name the method as expected."))
+		self.assertTrue(hasattr(q, 'main'), _("You did not have a main methode."))
+		self.assertTrue(hasattr(q, 'moyenne'), _("You did not have a moyenne methode."))
 
 	def test(self):
 		for test in TEST :
-			stud_ans = q.mirroir(test[0])
-			corr_ans = corr.mirroir(test[0])
-			self.assertEqual(stud_ans,corr_ans,test[1].format(test[0],stud_ans,corr_ans))
+			stu_ans = open("stu_ans.txt", 'w')
+			sys.stdout = stu_ans
+			with patch('builtins.input', side_effect=test):
+				q.main()
+			stu_ans.close()
+			corr_ans = open("corr_ans.txt", 'w')
+			sys.stdout = corr_ans
+			with patch('builtins.input', side_effect=test):
+				corr.main()
+			corr_ans.close()
+			corr_ans = open("corr_ans.txt", 'r')
+			correct = corr_ans.read()
+			stu_ans = open("stu_ans.txt", 'r')
+			stud = stu_ans.read()
+			self.assertEqual(correct, stud,_("Your output don't match the expected one for the entries : \n a = {} \n b = {} \n The expected output :\n {} \n Your output:\n {}").format(test[0],test[1],correct,stud))
+			corr_ans.close()
+			stu_ans.close()
 
 if __name__ == '__main__':
 	unittest.main()
