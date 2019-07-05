3fa00d4d5662d19e6c990263ccf9acd8ca6e87d9
fiset <alexandre.fiset@student.uclouvain.be>
2019-07-05 11:25:40 +0200

    add TP1 ex 5

--- TP6Ex2/src/TestQ.py
+++ TP6Ex2/src/TestQ.py
@@ -13,10 +13,10 @@ M = [[random.randint(80,101),random.randint(0,21)] for i in range(24)]
 
 class TestPageRank(unittest.TestCase):
 
-	def test_exist_inverse(self):
+	def test_exist_name(self):
 		self.assertTrue(hasattr(q, 'tempete'), _("You did not name the method as expected."))
 
-	def test_inverse(self):
+	def test(self):
 		stud_ans = q.tempete(M)
 		corr_ans = corr.tempete(M)
 		self.assertEqual(stud_ans,corr_ans,_("You did not return the rigth result for the matrix \n M = {} \n Your result : \n ans = {} \n The expected result : \n ans = {} ").format(M,stud_ans,corr_ans))
