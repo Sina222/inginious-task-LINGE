791542c057371d849f3b96aeed8fff6a4ae845f8
fiset <alexandre.fiset@student.uclouvain.be>
2019-07-05 10:31:03 +0200

    add TP6Ex2 + delete __pycache__ file in TP4EX8Supp

---
+++ TP6Ex2/src/TestQ.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+import unittest
+import q
+import CorrQ as corr
+import random
+
+#Creation of the random matrix M
+M = [[random.randint(80,101),random.randint(0,21)] for i in range(24)]
+
+
+class TestPageRank(unittest.TestCase):
+
+	def test_exist_inverse(self):
+		self.assertTrue(hasattr(q, 'tempete'), _("You did not name the method as expected."))
+
+	def test_inverse(self):
+		stud_ans = q.tempete(M)
+		corr_ans = corr.tempete(M)
+		self.assertEqual(stud_ans,corr_ans,_("You did not return the rigth result for the matrix \n M = {} \n Your result : \n ans = {} \n The expected result : \n ans = {} ").format(M,stud_ans,corr_ans))
+
+if __name__ == '__main__':
+	unittest.main()
